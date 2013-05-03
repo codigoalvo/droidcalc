31655875aab21896155e1fb4f19622e5cc9c5078
codigoalvo <codigoalvo@gmail.com>
2013-05-03 18:51:57 -0300

    Ajustes eclipse

--- src/codigoalvo/droidcalc/DroidCalc.java
+++ src/codigoalvo/droidcalc/DroidCalc.java
@@ -2,14 +2,19 @@ package codigoalvo.droidcalc;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.view.*;
+import android.widget.ListView;
 
 public class DroidCalc extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_droid_calc);
+		setContentView(R.layout.main);
+		
+		ListView contatosList = (ListView)findViewById(R.id.valores_list);
+		contatosList.setAdapter(new InputAdapter(this));
 	}
 
 	@Override
@@ -17,6 +22,11 @@ public class DroidCalc extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_droid_calc, menu);
 		return true;
+	}
+
+	public void onCalcular(View view) {
+		Intent calcIntent = new Intent(this, ResultadoActivity.class);
+		startActivity(calcIntent);
 	}
 
 }
